52b2504c56edfbfdf145f50cdfa14caa61630f06
renan.okada <rokada@uolinc.com>
2023-09-29 11:18:25 -0300

    Aula 22/09 - Widgets de navegação e input

--- bin/untitled.dart
+++ bin/untitled.dart
@@ -1,5 +1,15 @@
-import 'package:untitled/untitled.dart' as untitled;
+import 'package:flutter/material.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${untitled.calculate()}!');
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('Ola Flutter'),),
+          body: Text('Ola Flutter')
+      ),
+    );
+  }
 }
